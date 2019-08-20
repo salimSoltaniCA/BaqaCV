5b524ab2249acb4127a2ab2e223af112b7cc8de0
Soltani <salim.soltani@cgi.com>
2019-08-20 14:13:56 -0400

    Ajout la methode getProfiles dans HomeController

--- baQaCV/src/main/java/com/example/demo/controllers/HomeController.java
+++ baQaCV/src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,5 +1,30 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.pojos.Profile;
+import com.example.demo.services.IProfileRepository;
+
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@RestController
+@RequestMapping({"/api"})
 public class HomeController {
 
+	@Autowired
+	IProfileRepository profile;
+	
+	public HomeController() {
+		
+	}
+
+	@GetMapping("/greeting")
+	public List<Profile> getProfiles() {
+		return (List<Profile>) profile.findAll();
+	}
 }

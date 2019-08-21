530ca8cd89aaeb784b33dea1223e9fd423664914
Soltani <salim.soltani@cgi.com>
2019-08-21 11:38:14 -0400

    Add unit tests for profile authentification by Login and Password

--- baQaCV/src/test/java/com/example/demo/DemoApplicationTests.java
+++ baQaCV/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,9 +10,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DemoApplicationTests {
 
-	private static final Logger log = LoggerFactory.getLogger(DemoApplicationTests.class);
-	 
-    private final String SERVER_URL = "http://localhost";
+	private final String SERVER_URL = "http://localhost";
     private final String THINGS_ENDPOINT = "/things";
  
     private RestTemplate restTemplate;
